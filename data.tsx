83e858aabd997e5161d69fa8675dafe42a2d1f8e
Om Sharma <153528804+R0GDEV@users.noreply.github.com>
2025-03-20 12:10:54 +0530

    Update

---
+++ data.tsx
@@ -0,0 +1,23 @@
+const ticketData = [
+  {
+    id: 4,
+    source: {
+      stationCode: "S",
+      stationNameHindi: "नवाडे रोड",
+      stationNameEnglish: "NAVADE ROAD",
+      stationNameMarathi: "नवाडे रोड",
+    },
+    destination: {
+      stationCode: "D",
+      stationNameHindi: "पनवेल",
+      stationNameEnglish: "PANVEL",
+      stationNameMarathi: "पनवेल",
+    },
+    price: 5,
+    via: "KLMC",
+    distance: "8 km",
+    travelTime: 25,
+  },
+];
+
+export default ticketData;
