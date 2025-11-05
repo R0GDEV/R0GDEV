5493d5b90abf391cde092f77e901e23718e9487a
Om Sharma <153528804+R0GDEV@users.noreply.github.com>
2025-11-05 09:17:28 +0530

    Update stationRoutes.ts

--- stationRoutes.ts
+++ stationRoutes.ts
@@ -200,6 +200,25 @@
     },
     destination: {
       stationCode: "D",
+      stationNameHindi: "खोपोली",
+      stationNameEnglish: "KHOPOLI",
+      stationNameMarathi: "खोपोली",
+    },
+    price: { SECOND: 20 , FIRST: 115 },
+    via: "KYN",
+    distance: "72 km",
+    travelTime: 45,
+  },
+{
+    id: 14,
+    source: {
+      stationCode: "S",
+      stationNameHindi: "दिवा",
+      stationNameEnglish: "DIVA",
+      stationNameMarathi: "दिवा",
+    },
+    destination: {
+      stationCode: "D",
       stationNameHindi: "टिटवाला",
       stationNameEnglish: "TITWALA",
       stationNameMarathi: "टिटवाळा",
@@ -248,25 +267,7 @@
     travelTime: 25,
   },
 
-{
-    id: 15,
-    source: {
-      stationCode: "S",
-      stationNameHindi: "दिवा",
-      stationNameEnglish: "DIVA",
-      stationNameMarathi: "दिवा",
-    },
-    destination: {
-      stationCode: "D",
-      stationNameHindi: "खोपोली",
-      stationNameEnglish: "KHOPOLI",
-      stationNameMarathi: "खोपोली",
-    },
-    price: { SECOND: 20 , FIRST: 115 },
-    via: "KYN",
-    distance: "72 km",
-    travelTime: 45,
-  },
+
  
  
  
